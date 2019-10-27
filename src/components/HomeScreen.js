6152d21cde7329a29b7f7c3c62475d8596743f08
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-10-27 08:25:16 +0000

    add instructions

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -5,6 +5,15 @@ export default class HomeScreen extends Component {
     return (
       <div className="content_box">
         <h3>Welcome!</h3>
+        <div id="instructions">
+          <h4>How To Use:</h4>
+          <span className="num">1.</span><span>Add User</span><br/>
+          <span className="num">2.</span><span>Verify Email</span><br/>
+          <span className="num">3.</span><span>Login</span><br/>
+          <span className="num">4.</span><span>Add Item</span><br/>
+          <span className="num">5.</span><span>Search For Items</span><br/>
+          <span className="num">6.</span><span>Logout</span><br/>
+        </div>
       </div>
     );
   }
