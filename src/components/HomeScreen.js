2ffdc2741c9f225643403e3f57c5b6a051140a88
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-10-27 08:58:52 +0000

    more css

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -4,15 +4,15 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <div className="content_box">
-        <h3>Welcome!</h3>
+        <h2>Welcome!</h2>
         <div id="instructions">
           <h4>How To Use:</h4>
-          <span className="num">1.</span><span>Add User</span><br/>
-          <span className="num">2.</span><span>Verify Email</span><br/>
-          <span className="num">3.</span><span>Login</span><br/>
-          <span className="num">4.</span><span>Add Item</span><br/>
-          <span className="num">5.</span><span>Search For Items</span><br/>
-          <span className="num">6.</span><span>Logout</span><br/>
+          <span className="num">1.</span><span className="instruction">Add User</span><br/>
+          <span className="num">2.</span><span className="instruction">Verify Email</span><br/>
+          <span className="num">3.</span><span className="instruction">Login</span><br/>
+          <span className="num">4.</span><span className="instruction">Add Item</span><br/>
+          <span className="num">5.</span><span className="instruction">Search For Items</span><br/>
+          <span className="num">6.</span><span className="instruction">Logout</span><br/>
         </div>
       </div>
     );
