71030e5359722a672e0b2ff50895d97749a3a7b2
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2024-02-03 06:51:33 -0500

    change home screen

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -6,11 +6,10 @@ export default class HomeScreen extends Component {
       <div>
         <h2>Welcome!</h2>
         <div id="instructions">
-          <h4>Milestone 2 Items:</h4>
-          <span className="num">1.</span><span className="instruction">New search options under Search tab</span><br/>
-          <span className="num">2.</span><span className="instruction">Follow under Follow tab</span><br/>
-          <span className="num">3.</span><span className="instruction">Delete item under Item tab (after searching)</span><br/>
-          <span className="num">4.</span><span className="instruction">User GET requests under User tab (after searching)</span><br/>
+          <h4>TODO</h4>
+          <span className="num">1.</span><span className="instruction">Make better UI</span><br/>
+          <span className="num">2.</span><span className="instruction">????</span><br/>
+          <span className="num">3.</span><span className="instruction">Profit!</span><br/>
         </div>
       </div>
     );
